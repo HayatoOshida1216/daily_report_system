b6a9337c54db072b9a422bb54b64b74ce51c1cd0
oshidahayato <oshidahayato@192.168.0.13>
2021-12-05 15:47:22 +0900

    EmployeeValidator、EmployeeService作成

--- src/main/java/models/validators/EmployeeValidator.java
+++ src/main/java/models/validators/EmployeeValidator.java
@@ -12,13 +12,11 @@ public class EmployeeValidator {
             EmployeeService service, EmployeeView ev, Boolean codeDuplicateCheckFlag, Boolean passwordCheckFlag) {
         List<String> errors = new ArrayList<String>();
 
-        //社員番号のチェック
         String codeError = validateCode(service, ev.getCode(), codeDuplicateCheckFlag);
         if (!codeError.equals("")) {
             errors.add(codeError);
         }
 
-        //氏名のチェック
         String nameError = validateName(ev.getName());
         if (!nameError.equals("")) {
             errors.add(nameError);
@@ -34,23 +32,18 @@ public class EmployeeValidator {
 
     private static String validateCode(EmployeeService service, String code, Boolean codeDuplicateCheckFlag) {
 
-        //入力値がなければエラーメッセージを返却
         if (code == null || code.equals("")) {
             return MessageConst.E_NOEMP_CODE.getMessage();
         }
 
         if (codeDuplicateCheckFlag) {
-            //社員番号の重複チェックを実施
 
             long employeesCount = isDuplicateEmployee(service, code);
 
-            //同一社員番号が既に登録されている場合はエラーメッセージを返却
             if (employeesCount > 0) {
                 return MessageConst.E_EMP_CODE_EXIST.getMessage();
             }
         }
-
-        //エラーがない場合は空文字を返却
         return "";
     }
     private static long isDuplicateEmployee(EmployeeService service, String code) {
@@ -64,17 +57,13 @@ public class EmployeeValidator {
             return MessageConst.E_NONAME.getMessage();
         }
 
-        //入力値がある場合は空文字を返却
         return "";
     }
     private static String validatePassword(String password, Boolean passwordCheckFlag) {
 
-        //入力チェックを実施 かつ 入力値がなければエラーメッセージを返却
         if (passwordCheckFlag && (password == null || password.equals(""))) {
             return MessageConst.E_NOPASSWORD.getMessage();
         }
-
-        //エラーがない場合は空文字を返却
         return "";
     }
 }
